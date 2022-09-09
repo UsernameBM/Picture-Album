fe9ef98ebb2398543d1a539eeca20258cc21b120
stefandreas <90840165+stefandreas@users.noreply.github.com>
2022-09-09 15:51:57 +0200

    New script add-picture for choosing images that display on the page. Albumbutton now takes you to your-picture.html. New three dot dropdown menu for all html-files. Stefan, James, Anuka, Henning

--- javascript/add-album.js
+++ javascript/add-album.js
@@ -1,15 +1,24 @@
 function newAlbum(){
     
-    console.log("Hej hejjjjjjj");
-
-
     let divElement = document.createElement("div");
     divElement.className='album-flexbox';
     let imgElement = document.createElement("img");
     imgElement.className='img';
     imgElement.src = "/img/ailbhe-flynn-jkZs3Oi9pq0-unsplash.jpg";
-    divElement.appendChild(imgElement);
+
+    let buttonElement = document.createElement("button");
+    buttonElement.className = "btn";
+    buttonElement.type = "button";
+    
+    buttonElement.onclick = yourPicHtml;
+    function yourPicHtml(){
+        window.location.assign("/html/your-pictures.html");
+    }
+    buttonElement.appendChild(imgElement);
+    
     let element = document.getElementById("album-flexbox-holder");
+    divElement.appendChild(buttonElement);
+
     element.appendChild(divElement);
     
 } 
--- javascript/add-picture.js
+++ javascript/add-picture.js
@@ -1,14 +1,43 @@
-function uploadPictureFunction(){
-    //<input type="file" name="file" id="file"></input>
+var input = document.querySelector('input');
+var albumHolder = document.querySelector('.album-flexbox-holder');
+
+//input.style.opacity = 0;
+input.addEventListener('change', createPictureList);
+
+function createPictureList() {
     
-    const selectedFile = document.getElementById('file').files[0];
-    document.getElementById("album-flexbox").
-    appendChild(`<img class="img" src="${selectedFile.result}" title="${selectedFile.name}"/>`);
-    output.innerHTML = "selectedFile";
-    const pictureArray = [];
-    pictureArray.push("file");
-    
-    
-    //const divPicture = document.createElement("div");
-    //divPicture.files = selectedFile;
-} 
+    while(albumHolder.firstChild) {
+      albumHolder.removeChild(albumHolder.firstChild);
+    }
+  
+    const currentFiles = input.files;
+    if (currentFiles.length === 0) {
+      const paragraph = document.createElement('p');
+      paragraph.textContent = 'No files currently selected for upload';
+      albumHolder.appendChild(paragraph);
+    } else {
+     
+      for (const file of currentFiles) {
+        let divElement = document.createElement("div");
+        divElement.className='album-flexbox';
+        const paragraph = document.createElement('p');
+        
+        paragraph.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+        const image = document.createElement('img');
+        image.src = URL.createObjectURL(file);
+        divElement.appendChild(image);
+        divElement.appendChild(paragraph);
+         
+        albumHolder.appendChild(divElement);
+      }
+    }
+  }
+  function returnFileSize(number) {
+    if(number < 1024) {
+      return number + 'bytes';
+    } else if(number > 1024 && number < 1048576) {
+      return (number/1024).toFixed(1) + 'KB';
+    } else if(number > 1048576) {
+      return (number/1048576).toFixed(1) + 'MB';
+    }
+  }
